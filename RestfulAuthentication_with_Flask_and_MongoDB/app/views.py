3f3200e23504b074764fa28fd29ea9db9f677e06
Eb <ibbad@users.noreply.github.com>
2015-12-29 19:09:15 +0200

    Update views.py

--- RestfulAuthentication_with_Flask_and_MongoDB/app/views.py
+++ RestfulAuthentication_with_Flask_and_MongoDB/app/views.py
@@ -2,6 +2,8 @@ from flask import request, abort, jsonify, g
 from . import api
 from .models import User
 from flask.ext.httpauth import HTTPBasicAuth
+import random
+import string
 
 auth = HTTPBasicAuth()
 
@@ -42,9 +44,7 @@ def new_user():
 @api.route('/api/users/<int:id>')
 @auth.login_required
 def get_user(id):
-    print "here"
     user = User.objects(userid=id).first()
-    print "here2"
     if user is None:              # no user found
         abort(400)
     return jsonify({'username': user.username})
@@ -57,9 +57,58 @@ def get_resource():
 @api.route('/api/token')
 @auth.login_required            # username and password required.
 def get_auth_token():
-    print "here1"
     token = g.user.generate_auth_token(600)
-    print "here2"
     return jsonify({'token': token.decode('ascii'), 'duration': 600})
 
+@api.route('/api/user/changepwd', methods=['POST'])
+@auth.login_required
+def change_password():
+    user = g.user
+    new_password = request.json.get('password')
+    user.hash_password(new_password)
+    user.save()
+    return jsonify({'username': user.username, 'password_changed': 'success'}), 202
 
+@api.route('/api/user/request_reset_password', methods=['POST'])
+def get_reset_token():
+    username = request.json.get('username')
+    if username is None:
+        abort(400)          # no username provided.
+    user = User.objects(username=username).first()
+    if user is None:
+        abort(400)          # no user found.
+    reset_token = user.generate_reset_token()
+    return jsonify({'reset-password-token': reset_token.decode('ascii'), 'duration': '1 day'})
+
+@api.route('/api/user/v1.0/reset_password', methods=['POST'])
+def reset_password_v1():
+    """
+    When user gets a randomly generated password upon request for password change.
+    :return:
+    """
+    reset_token = request.json.get('rst_token')
+    user = User.verify_reset_token(reset_token)
+    if user is None:
+        abort(400)          # not a valid user
+    # Generate a random password and send it back to the user.
+    random_pwd = ''.join(random.SystemRandom().
+                         choice(string.ascii_uppercase+string.digits+string.ascii_lowercase)
+                         for _ in range(16))
+    user.hash_password(random_pwd)
+    user.save()
+    return jsonify({'new_password': random_pwd}), 201
+
+@api.route('/api/user/v1.1/reset_password', methods=['POST'])
+def reset_password_v2():
+    """
+    When user provides the new password.
+    :return:
+    """
+    reset_token = request.json.get('rst_token')
+    new_password = request.json.get('password')
+    user = User.verify_reset_token(reset_token)
+    if user is None:
+        abort(400)          # not a valid user
+    user.hash_password(new_password)
+    user.save()
+    return jsonify({'password_changed': 'success'}), 201
